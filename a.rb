67897ed008f5a8f92abac09524659d791c720b06
Sophie <sophie4ngo@gmail.com>
2018-09-13 01:19:27 -0700

    learn sub, split, strip

--- a.rb
+++ a.rb
@@ -41,16 +41,51 @@
 # end 
 # # A variable that is available to that Class !!
 
-# RUBY STRINGS  
+# # RUBY STRINGS  
 
-puts "Name an animal" 
-animal = gets.chomp 
+# puts "Name an animal" 
+# animal = gets.chomp 
 
-puts "Name a noun"
-noun = gets.chomp
+# puts "Name a noun"
+# noun = gets.chomp
 
-p "The quick brown #{animal} jumped over the lazy #{noun}"
+# p "The quick brown #{animal} jumped over the lazy #{noun}"
 
 
 # String Manipulation 
+
+p "Astros".upcase 
+p "Milk".upcase
+
+m = "Milk".upcase
+p m
+
+g = "GOODPANDA".downcase
+p g
+
+# String substitution 
+
+str = "The quick brown dox jumped over the quick dog"
+
+p str.sub "quick", "slow" # this replaces the first quick to slow 
+p str.gsub "quick", "slow" # this repalces all the quick words to slow 
+p str # this does not change the string value 
+p str.gsub! "quick", "slow" #this replaces all the words and it permanently changes the sentence later on too
+
+# Split and Strip Method 
+
+string = "Having the oportunity to go to school is a priviledge   "
+
+# Strip 
+p string.strip
+
+# Split 
+
+p string.split.size 
+p string.split(//).size 
+
+
+
+
+
 
