1da00e9607e1f35748e0b5b532407cffb32f8846
Sophie <sophie4ngo@gmail.com>
2018-09-13 00:24:42 -0700

    first commit

---
+++ a.rb
@@ -0,0 +1,56 @@
+# # RuUBY VARIABLES 
+
+# puts [1, 2, 3, 4]
+# p [1, 2, 3, 4]
+
+# puts "What is your password?" 
+# password = gets.chomp
+
+# if password == "MoBear"
+#   puts true
+# else
+#   puts false 
+# end 
+
+# #Local Variables 
+# 10.times do 
+#   x = 10 
+# end 
+
+# # Global Variables 
+
+# 10.times do 
+#   $x = 10
+# end 
+
+# p $x
+
+# # Instance Variables 
+# @batt_average = 300 
+# # used in html @jobs.each do |jobs|
+
+# # Constant 
+# TEAM = "Angels"
+# TEAM= "Athletics"
+
+# p TEAM
+
+# # Class Variable 
+# class MyClass 
+#   @@teams = ["A's", "Tigers"]
+# end 
+# # A variable that is available to that Class !!
+
+# RUBY STRINGS  
+
+puts "Name an animal" 
+animal = gets.chomp 
+
+puts "Name a noun"
+noun = gets.chomp
+
+p "The quick brown #{animal} jumped over the lazy #{noun}"
+
+
+# String Manipulation 
+
